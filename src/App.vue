f9ed73422ce432fcbdfcbeb81bd21822b901b589
Carsten Ruhl <ToxicGecko13@Gmail.com>
2019-03-26 13:07:00 -0700

    Starting Code

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <img alt="Active Martial Arts Logo" src="./assets/ActiveLogo.png">
+    <ul class="AwardsList">
+      <award-card 
+        v-for="award in awards" 
+        v-bind:key="award.id" 
+        v-bind:award="award" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import AwardCard from './components/AwardCard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AwardCard
+  },
+  data () {
+    return {
+      awards: [
+        {
+          id: 1,
+          title: 'High Kick',
+          detail: 'Kick over your head 10 times in under 10 seconds.',
+          imageurl: require('./assets/clouds-fight-jumping-62376.jpg')
+        },
+        {
+          id: 2,
+          title: 'Closed Fist',
+          detail: 'Break a 1 inch wood board with closed fist.',
+          imageurl: require('./assets/aggressive-anger-angry-163431.jpg')
+        },
+        {
+          id: 3,
+          title: 'Dragon Spirit',
+          detail: 'Complete Your Form 100 Times',
+          imageurl: require('./assets/bokeh-daylight-dragon-208326.jpg')
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background: linear-gradient(
+    rgba(200, 200, 200, 0.6),
+    rgba(80, 80, 80, 0.7)
+  )
+}
+
+.AwardsList {
+  list-style-type: none;
+}
+</style>
---
+++ src/components/AwardCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <li class="card">
+        <img v-bind:src="award.imageurl" style="width:100%">
+        <div class="container">
+            <h4><b>{{ award.title }}</b></h4> 
+            <p>{{ award.detail }}</p> 
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+        props: {
+            award: {
+                type: Object,
+                required: true
+            }
+        }
+};
+</script>
+
+<style>
+    .card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  max-width: 500px;
+  margin: 10px;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
